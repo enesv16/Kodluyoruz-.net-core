a60ac445256e78b17e32d0ee6204447183e75e00
Muhammet Enes Vardar <63022959+enesv16@users.noreply.github.com>
2024-05-01 23:10:41 +0300

    enum

---
+++ Patika-dotnet/enum/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    // Enes Vardar
+    // Sirali verilerde kullaniliyor, ardişik indexlerde
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Gunler.Pazar);
+        Console.WriteLine((int)Gunler.Cumartesi);
+
+        int sicaklik = 32;
+
+        if (sicaklik <= (int)Hava_Durumu.Normal)
+            Console.WriteLine("Dişariya çikmak için havanin biraz daha isinmasini bekleyelim");
+        else if (sicaklik >= (int)Hava_Durumu.Sicak)
+            Console.WriteLine("Dişariya çikmak için çok sicak bir gün");
+        else if (sicaklik >= (int)Hava_Durumu.Normal && sicaklik <= (int)Hava_Durumu.Cok_Sicak)
+            Console.WriteLine("Hadi dişariya çikalim");
+    }
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Sali,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum Hava_Durumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        Cok_Sicak = 30
+    }
+}
