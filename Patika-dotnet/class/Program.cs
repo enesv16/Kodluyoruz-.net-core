a71b1639f0ecb864a701633b9796b14987e6d713
Muhammet Enes Vardar <63022959+enesv16@users.noreply.github.com>
2024-05-01 17:14:31 +0300

    class

---
+++ Patika-dotnet/class/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calisan calisan1 = new Calisan();
+        calisan1.ad="Enes";
+        calisan1.soyad="Vardar";
+        calisan1.yas=30;
+        calisan1.maas=5000;
+        Console.WriteLine("Calisan Bilgileri");
+        calisan1.bilgiGoster();
+    }
+}
+
+
+class Calisan
+{
+    public string ad;
+    public string soyad;
+    public int yas;
+    public int maas;
+
+
+    public void bilgiGoster(){
+        Console.WriteLine("Calisan Adi: " + this.ad);
+        Console.WriteLine("Calisan Soyadi:  " + this.soyad);
+        Console.WriteLine("Calisan Yasi: " + this.yas);
+        Console.WriteLine("Calisan Maasi: " + this.maas);
+    }
+}
