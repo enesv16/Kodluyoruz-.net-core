07367515ec20ca3680651111bd8ac0d6d7b4d4ae
Muhammet Enes Vardar <63022959+enesv16@users.noreply.github.com>
2024-05-01 18:35:13 +0300

    kurucu metotlar yapıldı

---
+++ Patika-dotnet/constructor-methods/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Calisan calisan1 = new Calisan();
+        calisan1.Ad = "Enes";
+        calisan1.Soyad = "Vardar";
+        calisan1.Yas = 30;
+        calisan1.Maas = 5000;
+        Console.WriteLine("Calisan Bilgileri");
+        calisan1.bilgiGoster();
+        Console.WriteLine("*********************");
+
+
+        Calisan calisan2 = new Calisan("Dilara", "Sevim", 19, 77000);
+        Console.WriteLine("Calisan Bilgileri");
+        calisan2.bilgiGoster();
+    }
+}
+
+class Calisan
+{
+    public string Ad;
+    public string Soyad;
+
+    public int Yas ;
+
+    public int Maas;
+    public Calisan(string ad, string soyad, int yas, int maas)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+        this.Yas = yas;
+        this.Maas = maas;
+    }
+    public Calisan() { }
+
+    public void bilgiGoster()
+    {
+        Console.WriteLine($"Ad : {this.Ad} \nSoyad : {this.Soyad}\nYas : {this.Yas} \nMaas : {this.Maas}");
+    }
+    
+}   
